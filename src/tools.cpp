5459ade4fcd582c0ff32531e0f5212f609f51b75
silenc3502 <gcccompil3r@gmail.com>
2018-02-02 11:45:05 +0900

    Add Code of EKF Lecture 22

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,31 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  /* It comes from lecture 22 of EKF. */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if(estimations.size() == 0)
+  {
+    cout << "input is empty" << endl;
+    return rmse;
+  }
+
+  if(estimations.size() != ground_truth.size())
+  {
+    cout << "Invalid estimation or ground truth. Data should have the same size" << endl;
+    return rmse;
+  }
+
+  for(unsigned int i = 0; i < estimations.size(); i++)
+  {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
